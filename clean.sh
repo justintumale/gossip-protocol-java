09c6d36464acd618b731d05652b867deaed1c94e
justintumale <fjetumale@yahoo.com>
2017-08-14 20:08:45 -0700

    added script for compilation and cleaning

---
+++ clean.sh
@@ -0,0 +1,1 @@
+rm gossip_protocol/src/*.class
---
+++ compile.sh
@@ -0,0 +1,7 @@
+javac gossip_protocol/src/Logger.java
+javac gossip_protocol/src/GossipperThread.java
+javac gossip_protocol/src/WorkerThread.java
+javac gossip_protocol/src/Member.java
+javac gossip_protocol/src/ListenerThread.java
+javac gossip_protocol/src/Node.java
+javac gossip_protocol/src/NodeClient.java
